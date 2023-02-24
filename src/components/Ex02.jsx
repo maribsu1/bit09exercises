94325aa2370343419147a4472fa57b664529c18f
maribsu1 <maribsu2@gmail.com>
2023-02-24 16:41:43 -0500

    modificacion ex02

--- src/components/Ex02.jsx
+++ src/components/Ex02.jsx
@@ -1,54 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { Button, Card, Spinner } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 export const Ex02 = () => {
   const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://random-d.uk/api/v2/random")
       .then((response) => response.json())
-      .then((data) => {
-        setImageUrl(data.url);
-        setLoading(false);
-      });
+      .then((data) => setImageUrl(data.url));
   }, []);
 
   const handleClick = () => {
-    setLoading(true);
     fetch("https://random-d.uk/api/v2/random")
       .then((response) => response.json())
-      .then((data) => {
-        setImageUrl(data.url);
-        setLoading(false);
-      });
+      .then((data) => setImageUrl(data.url));
   };
 
   return (
     <div>
       <br />
       <h1>Llamar una API</h1>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ marginTop: "50px" }}
-      >
-        {loading ? (
-          <Spinner animation="border" variant="primary" />
-        ) : (
-          <Card className="text-center" bg="secondary" style={{ height: "100%" }}>
-            <Card.Img
-              variant="top"
+      <div className="d-flex justify-content-center align-items-center" style={{ marginTop: "50px" }}>
+        <Card className="text-center" bg="secondary">
+          <div style={{ width: "100%", height: "auto", textAlign: "center" }}>
+            <img
               src={imageUrl}
-              style={{ width: 300, height: 250, margin: "auto" }}
+              alt="Imagen aleatoria"
+              style={{ maxWidth: "100%", maxHeight: "200px" }}
             />
-            <Card.Body>
-              <Button variant="dark" onClick={handleClick}>
-                Cambiar imagen
-              </Button>
-              <br />
-            </Card.Body>
-          </Card>
-        )}
+          </div>
+          <Card.Body>
+            <Button variant="dark" onClick={handleClick}>
+              Cambiar imagen
+            </Button>
+            <br />
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
